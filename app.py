e1c5b4417267d463e5d4a426226966d742ca28f5
tonghengkeng <keng.tongheng14@gmail.com>
2024-07-20 11:20:37 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# print("Hello")
+# print("Hi")
+
+# print("Single 'quote' inside double quote")
+# print("Double 'quote' inside double quote")
+# print("single \"quote\" and \"double\" quote inside quote")
+
+# name=input("what is your name ")
+# print("welcome " + name)
+
+# qty1=int(input("Qty1 = "))
+# qty2=int(input("Qty2 = "))
+# price=float(input("Price ="))
+# total_qty=qty1+qty2
+# print("Total Qty= "+str(total_qty))
+# amount=total_qty*price
+# print("Total Amount= "+str(amount))
+
+
